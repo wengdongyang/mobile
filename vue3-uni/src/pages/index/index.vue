e3cc12dea69ce570a0ddf1db04d48a455751458a
wdy <wdy@mini.com>
2024-08-15 14:45:18 +0800

    feat: create

---
+++ vue3-uni/src/pages/index/index.vue
@@ -0,0 +1,23 @@
+<!-- @format -->
+<template>
+  <view class="index-layout">
+    <uv-icon name="photo"></uv-icon>
+    {{ title }}
+  </view>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+import { get, set } from '@vueuse/core';
+import { onLoad, onShow, onHide } from '@dcloudio/uni-app';
+// apis
+// hooks
+// utils
+// stores
+// configs
+// components
+const title = ref('Hello');
+</script>
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
