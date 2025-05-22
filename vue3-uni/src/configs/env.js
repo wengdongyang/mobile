6d767a49ef4a04b6cd58c5a47be882f6e46f3cce
wdy <wdy@pro.com>
2025-05-22 16:36:00 +0800

    fix：暂存代码

--- vue3-uni/src/configs/env.js
+++ vue3-uni/src/configs/env.js
@@ -1,18 +1,19 @@
 // #ifdef MP-WEIXIN
 // 微信小程序
 const envVersion = __wxConfig.envVersion;
-const fileServerPath = `${import.meta.env.VITE_LOCATION_ORIGIN}${import.meta.env.VITE_FILE_UPLOAD_PATH}`;
 // #endif
 // #ifdef H5
 // 浙政钉H5
 const envVersion = 'release';
-const fileServerPath = `../`;
 // #endif
+
+const testRequestBaseUrl = `${import.meta.env.VITE_TEST_REQUEST_BASE_URL}${VITE_TEST_REQUEST_BASE_ROUTER}/`;
+
+const prodRequestBaseUrl = `${import.meta.env.VITE_PROD_REQUEST_BASE_URL}${VITE_PROD_REQUEST_BASE_ROUTER}/`;
 
 const fileSourcePath = `${import.meta.env.VITE_FILE_SOURCE_PATH}`;
 
 export const ENV = {
   ENV_VERSION: envVersion,
-  FILE_SERVER_PATH: fileServerPath,
   FILE_SOURCE_PATH: fileSourcePath,
 };
