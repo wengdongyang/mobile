13348740090cfa26bee834306ccaae95519da8cf
wdy <wdy@mini.com>
2025-06-17 18:56:10 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -11,22 +11,12 @@ import { useEnv } from '@src/hooks';
 // components
 const { initEnv } = useEnv();
 // 未格式化的 JS
-function example() {
-  console.log('原始格式');
-  const obj = { a: 1, b: 2 };
-  return obj;
-}
 
 onLaunch(() => {
-  console.info('App Launch');
   console.info('App Launch');
   initEnv();
 });
 onShow(() => {
-  console.info('App Show');
-
-  console.info('App Show2');
-
   console.info('App Show');
 });
 onHide(() => {
